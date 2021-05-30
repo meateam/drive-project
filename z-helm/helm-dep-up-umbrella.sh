63647496a8734b63e79c4c5a9cb868f0fe0f3a4a
yoavrozner <rozner.yoav@gmail.com>
2021-05-30 11:26:26 +0300

    fix: update changed only charts and local charts only

--- z-helm/helm-dep-up-umbrella.sh
+++ z-helm/helm-dep-up-umbrella.sh
@@ -2,16 +2,24 @@
 
 path=$1
 cd $path
+
+updateSubDir() {
+  subDir=$1
+  echo "Updating dependencies in "$(pwd)"/"$subDir" ..."
+  rm -rf "$(pwd)"/"$subDir"/charts
+  helm dep up --skip-refresh "$(pwd)"/"$subDir"
+  echo
+}
+
+changedHelmDirs=$(git diff --name-only | xargs -L1 dirname | uniq)
+changedHelmDirs=${changedHelmDirs//z-helm/}
 if [ -e requirements.yaml ]; then
   for subDir in $(awk -F'repository: file://' '{print $2}' requirements.yaml)
   do
-    echo "Updating dependencies in "$(pwd)"/"$subDir" ..."
-    rm -rf "$(pwd)"/"$subDir"/charts
-    helm dep up "$(pwd)"/"$subDir"
-    echo
+    parsedSubDir=${subDir//../}
+    if [[ $changedHelmDirs == *$parsedSubDir* ]] || [[ -z $changedHelmDirs ]]; then
+      updateSubDir $subDir
+    fi
   done
-  echo "Updating dependencies in "$(pwd)" ..."
-  rm -rf "$(pwd)"/charts
-  helm dep up "$(pwd)"
-  echo
+  updateSubDir "."
 fi
